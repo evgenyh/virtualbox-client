005b4093001dd14df386d7c45d1b744d10ab2a60
Eugene <Eugene@_-_.local>
2019-05-13 13:34:33 +0300

    :)

--- src/main/java/net/honeyflower/virtualbox/client/event/NoOpEventProcessor.java
+++ src/main/java/net/honeyflower/virtualbox/client/event/NoOpEventProcessor.java
@@ -2,6 +2,11 @@ package net.honeyflower.virtualbox.client.event;
 
 import org.virtualbox_6_0.IEvent;
 
+/**
+ * this implementation silently discards all events
+ * @author Eugene
+ *
+ */
 public class NoOpEventProcessor implements EventProcessor {
 
 	@Override
